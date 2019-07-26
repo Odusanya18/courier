8d18ebd98280b5f73ea56413f857d32491c61023
Odusanya Victor <odusanya18@gmail.com>
2019-07-26 13:32:45 +0100

    configure reverse proxy in index

--- public/index.php
+++ public/index.php
@@ -12,16 +12,18 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
+// tell Symfony about your reverse proxy
+Request::setTrustedProxies(
+// the IP address (or range) of your proxy
+    ['192.0.0.1', $request->server->get('REMOTE_ADDR')],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
