1d6dfd57d6e0559008a129ec2110542ba2124247
Odusanya Victor <odusanya18@gmail.com>
2019-07-26 14:20:55 +0100

    configure cloudflare ssl better

--- public/index.php
+++ public/index.php
@@ -15,21 +15,17 @@ if ($_SERVER['APP_DEBUG']) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 
-if (!$request->server->has('APP_DEBUG')) {
-    $request->server->set('HTTPS', 'on');
-}
-
 // tell Symfony about your reverse proxy
 Request::setTrustedProxies(
 // the IP address (or range) of your proxy
-    ['127.0.0.1', '172.0.0.0/8'],
+    ['192.0.0.1', $request->server->get('REMOTE_ADDR')],
+
     // trust *all* "X-Forwarded-*" headers
     Request::HEADER_X_FORWARDED_ALL
-// or, if your proxy instead uses the "Forwarded" header
-// Request::HEADER_FORWARDED
-// or, if you're using AWS ELB
-// Request::HEADER_X_FORWARDED_AWS_ELB
 );
+if (!$request->server->has('APP_DEBUG')) {
+    $request->server->set('HTTPS', 'on');
+}
 
 $response = $kernel->handle($request);
 $response->send();
