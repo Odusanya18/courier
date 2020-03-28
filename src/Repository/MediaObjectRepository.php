1ce14b9829ccc62f5ec69a233c4cc684aa6130ea
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2020-03-28 15:25:43 +0100

    composer upgrade
    Signed-off-by: Victor Odusanya <victor.odusanya@interswitchgroup.com>

--- src/Repository/MediaObjectRepository.php
+++ src/Repository/MediaObjectRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\MediaObject;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method MediaObject|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MediaObjectRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MediaObject::class);
     }
